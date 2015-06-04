3d240e9ec4660631e7a3ca1e89fb97f3763a957b
golovko_yakov <golovko.yakov@gmail.com>
2015-06-04 23:48:50 +0300

    added *.sql file (database structure)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 /**
  * Created by JACOB on 02.06.2015.
  */
+// java -jar PortableExecutableCrawler.jar <host> <user> <password> <databaseName> <locationToFilesSaving>
+// example: java -jar PortableExecutableCrawler.jar 127.0.0.1 root root pefilesdb C:\Users\JACOB\Desktop
 public class Main {
     public static void main(String[] args) {
 
