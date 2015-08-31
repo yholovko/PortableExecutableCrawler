c26c79430556fe82c8ba8e0905019288f16278f1
Jacob <golovko.yakov@gmail.com>
2015-09-01 02:09:56 +0300

    convert for the new Google Play html tags

--- src/main/java/googleplay/com/ProducerLinks.java
+++ src/main/java/googleplay/com/ProducerLinks.java
@@ -10,11 +10,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -36,15 +35,21 @@ public class ProducerLinks implements Runnable {
 
         apkFile.setUrl(Constants.GOOGLEPLAY_COM + url);
         apkFile.setLocation(Constants.LOCATION_TO_FILES_SAVING_APK);
-        apkFile.setName(doc.select("div > div.details-info > div.info-container > div.document-title > div").text());
+        apkFile.setName(doc.select("div > div > div.main-content > div > div > div.details-info > div > div.info-box-top > h1 > div").text());
         apkFile.setVersion(doc.select("div > div.details-section-contents > div:nth-child(4) > div.content").text());
-        apkFile.setCategory(doc.select("div > div.details-info > div.info-container > div:nth-child(3) > a > span").text());
+        apkFile.setCategory(doc.select("div > div > div.main-content > div > div > div.details-info > div > div.info-box-top > h1 > div").text());
         apkFile.setDescription(doc.select("div.show-more-content.text-body > div.id-app-orig-desc").text());
 
-        apkFile.setSimilarAppsUrl(doc.select("div > div.details > h2 > a").stream().map(similarUrl -> Constants.GOOGLEPLAY_COM + similarUrl.attr("href")).collect(Collectors.toList()));
-        apkFile.setPrice(doc.select("#body-content > div.outer-container > div > div.main-content > div:nth-child(1) > div > div.details-info > div.info-container > div.details-actions > span > span > button > span:nth-child(3)").text());
+        apkFile.setSimilarAppsUrl(getSimilarAppsFor(url.replaceAll("details", "similar")));
+        apkFile.setPrice(doc.select("div.outer-container > div > div.main-content > div > div > div.details-info > div > div.info-box-bottom > div > div.details-actions-right > span > span > button > span:nth-child(3)").text());
 
         return apkFile;
+    }
+
+    private List<String> getSimilarAppsFor(String url){
+        WebDriver driver = GooglePlay.connectTo(Constants.GOOGLEPLAY_COM  + url, false);
+        Document doc = Jsoup.parse(driver.getPageSource());
+        return doc.select("div.outer-container > div > div.main-content > div > div > div > div > div > div > div > div.details > a.title").stream().map(similarUrl -> Constants.GOOGLEPLAY_COM + similarUrl.attr("href")).collect(Collectors.toList());
     }
 
     private void getApkFiles(Elements elements, int deepForSimilarApp) {
@@ -108,15 +113,15 @@ public class ProducerLinks implements Runnable {
             while (true) {
                 try {
                     Thread.sleep(1000);
-                    try {
-                        ((HtmlUnitDriver) driver).executeScript("document.getElementById('show-more-button').click()", new Object[]{""});
-                    }catch (WebDriverException e){
-                        APK_LOG.error(e);
-                    }
-                    Thread.sleep(6000);
+//                    try {
+//                        ((HtmlUnitDriver) driver).executeScript("document.getElementById('show-more-button').click()", new Object[]{""});
+//                    }catch (WebDriverException e){
+//                        APK_LOG.error(e);
+//                    }
+//                    Thread.sleep(6000);
 
                     Document doc = Jsoup.parse(driver.getPageSource());
-                    elements = doc.select("div > div.details > h2 > a");
+                    elements = doc.select("div > div > div.main-content > div > div > div > div.card-list.two-cards > div > div > div.details > a.title");
 
                     if (last == elements.size()) {
                         break;
@@ -132,6 +137,7 @@ public class ProducerLinks implements Runnable {
             getApkFiles(elements, Constants.DEEP_FOR_SIMILAR);
 
             APK_LOG.info(String.format("FINAL RESULT: Got %s applications from '%s' category", linksFromCategory, category));
+break;
         }
         try {
             goldenLinks.put(new ApkFile()); // LAST ELEMENT. 'Consumer' thread will stopped.
